84c7e88ee7f74d517124f3a48717dab478e14638
AliKhMahmoud <Ali.Mahmoud.135246@gmail.com>
2026-08-04 19:52:43 +0300

    ..

--- src/utils/sendEmail.js
+++ src/utils/sendEmail.js
@@ -2,13 +2,13 @@ const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
-  port: 587,
-  secure: false, 
+  port: 465,
+  secure: true, // <--- غيرناها لـ true واستخدمنا منفذ 465 الآمن
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS, 
   },
-  family: 4, 
+  family: 4 // <--- لتجبر السيرفر على استخدام IPv4 وتتجاوز مشكلة Render
 });
 
 const sendEmail = async (to, subject, text) => {
